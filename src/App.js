9dddda529ebebad4a024d698d87441d23bd8b4b0
NishcheyBhutani <nishchay.bhutani3010@gmail.com>
2021-03-18 13:57:07 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { add, sub, mult, div } from "./Calc";
+
+function App() {
+  return (
+
+    <>
+      <ul>
+        <li>{add(40, 4)}</li>
+        <li>{sub(40, 4)}</li>
+        <li>{mult(40, 4)}</li>
+        <li>{div(40, 4)}</li>
+
+      </ul>
+    </>
+  );
+}
+
+export default App;
